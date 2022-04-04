008bd8692abdc7d745ea1c2f79c37f324b2ad867
based <test@nein.com>
2022-04-04 11:09:23 +0200

    a

---
+++ test.lua
@@ -0,0 +1,31 @@
+local function digOnce()
+    if turtle.detect() then
+        turtle.dig()
+    end
+    turtle.forward()
+    if turtle.detectUp() then
+        turtle.digUp()
+    end
+    
+end
+
+local function digArm (length)
+    for i = 0, length, 1 do
+        digOnce()        
+    end
+    turtle.turnLeft()
+    turtle.turnLeft()
+    for i = 0, length, 1 do
+        turtle.forward()
+    end
+end
+local running = true
+while running do
+    digOnce()
+    turtle.turnLeft()
+    digArm(10)
+
+
+    
+end
+
